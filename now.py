a1b860983e5a1eb700a31add83ffab5cf64120e8
nuhs <58536161+nuhs@users.noreply.github.com>
2019-12-09 12:46:13 +0900

    Update now.py

--- now.py
+++ now.py
@@ -2,6 +2,12 @@
 
 '''MyFarmware'''
 
+from time import time, sleep
 from farmware_tools import device
+import cv2
 
 device.log(message='Now OK!', message_type='success')
+
+cap = cv2.VideoCapture(0)
+sleep(0.1)
+device.log('{}'.format(cap))
